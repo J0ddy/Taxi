ce7a69a12f7426ada87427b9e244173bb71576c1
J0ddy <me@joddy.dev>
2022-01-31 13:24:03 +0200

    Remove AuthWindow maximize box

--- Taxi/AuthWindow.Designer.cs
+++ Taxi/AuthWindow.Designer.cs
@@ -431,6 +431,7 @@
             this.ClientSize = new System.Drawing.Size(700, 550);
             this.Controls.Add(this.tabControlAuthWindow);
             this.FormStyle = MaterialSkin.Controls.MaterialForm.FormStyles.ActionBar_None;
+            this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(700, 550);
             this.Name = "AuthWindow";
             this.Padding = new System.Windows.Forms.Padding(3, 24, 3, 3);
