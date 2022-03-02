fea30478eac0069640cd8b5d1eab2045e572af26
J0ddy <me@joddy.dev>
2022-03-02 18:48:57 +0200

    Updated Structure Created User Interface

---
+++ Taxi/Model/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi.Model
+{
+    interface IUser
+    {
+        int Id { get; set; }
+        string UserName { get; set; }
+        string Password { get; set; }
+        string Email { get; set; }
+        string FistName { get; set; }
+        string LastName { get; set; }
+    }
+}
